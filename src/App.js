bf79dfa8b34dd2389a370b89eb0bcee66c82891f
Aaron Burzak <aaron.j.burzak@gmail.com>
2023-02-06 11:51:50 -0500

    added conditionals to ensure proper use of '0' and'.' buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useReducer } from "react";
 import DigitButton from "./DigitButton";
+import OperationButton from "./OperationButton";
 import "./styles.css";
 
 export const ACTIONS = {
@@ -17,6 +18,12 @@ export const ACTIONS = {
 function reducer(state,{ type, payload }){
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if (payload.digit === "0" && state.currentOperand === "0") {
+        return state
+      }
+      if (payload.digit === "." && state.currentOperand.includes(".")){
+        return state
+      } 
       return {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`
@@ -39,20 +46,20 @@ function App() {
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
-      {/* <DigitButton digit="/" dispatch={dispatch} /> */}
+      <OperationButton operation="/" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
       <DigitButton digit="3" dispatch={dispatch} />
-      {/* <DigitButton digit="*" dispatch={dispatch} /> */}
+      <OperationButton operation="*" dispatch={dispatch} />
       <DigitButton digit="4" dispatch={dispatch} />
       <DigitButton digit="5" dispatch={dispatch} />
       <DigitButton digit="6" dispatch={dispatch} />
-      {/* <DigitButton digit="+" dispatch={dispatch} /> */}
+      <OperationButton operation="+" dispatch={dispatch} />
       <DigitButton digit="7" dispatch={dispatch} />
       <DigitButton digit="8" dispatch={dispatch} />
       <DigitButton digit="9" dispatch={dispatch} />
-      {/* <DigitButton digit="-" dispatch={dispatch} /> */}
-      {/* <DigitButton digit="." dispatch={dispatch} /> */}
+      <OperationButton operation="-" dispatch={dispatch} />
+      <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
   
       <button className="span-two">=</button>
