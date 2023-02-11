676b526a4557dbb2c21a701c3b09a7dda5a56b98
Aaron Burzak <aaron.j.burzak@gmail.com>
2023-02-11 17:21:27 -0500

    finished INTEGER_FORMATTER, fixed bug when selecting '.' when currentOperand = null

--- src/App.js
+++ src/App.js
@@ -28,7 +28,8 @@ function reducer(state,{ type, payload }){
       if (payload.digit === "0" && state.currentOperand === "0") {
         return state
       }
-      if (payload.digit === "." && state.currentOperand.includes(".")){
+      
+      if (payload.digit === "." && state.currentOperand?.includes(".")){
         return state
       } 
       return {
@@ -69,6 +70,24 @@ function reducer(state,{ type, payload }){
         }
       case ACTIONS.CLEAR:
         return{}
+
+      case ACTIONS.DELETE_DIGIT:
+        if (state.overwrite){
+          return{
+            ...state,
+            overwrite: false,
+            currentOperand: null
+          }
+        }
+        if (state.currentOperand == null) return state
+        if (state.currentOperand.length === 1) {
+          return { ...state, currentOperand: null}
+        }
+        return{
+          ...state,
+          currentOperand: state.currentOperand.slice(0,-1)
+        }
+
       case ACTIONS.EVALUATE:
         if(state.operation == null || state.currentOperand == null || state.previousOperand == null){
           return state
@@ -107,6 +126,15 @@ function evaluate({ currentOperand, previousOperand, operation }) {
   return computation.toString()
 }
 
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits:0,
+})
+function formatOperand(operand){
+  if(operand == null) return
+  const [integer, decimal] = operand.split(".")
+  if (decimal == null) return INTEGER_FORMATTER.format(integer)
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+  }
 function App() {
   const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(
     reducer,
@@ -117,11 +145,11 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">{previousOperand} {operation}</div>
-        <div className="current-operand">{currentOperand}</div>
+        <div className="previous-operand">{formatOperand(previousOperand)} {operation}</div>
+        <div className="current-operand">{formatOperand(currentOperand)}</div>
       </div>
       <button className="span-two"  onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
-      <button>DEL</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
       <OperationButton operation="/" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
